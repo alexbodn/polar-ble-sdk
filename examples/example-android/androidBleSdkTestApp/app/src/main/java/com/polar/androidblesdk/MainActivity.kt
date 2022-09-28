1e81fbedf196e24d979b08dc518e91ad66eb81e0
abraham <alexbodn@gmail.com>
2022-09-28 23:02:59 +0300

    Update MainActivity.kt

--- examples/example-android/androidBleSdkTestApp/app/src/main/java/com/polar/androidblesdk/MainActivity.kt
+++ examples/example-android/androidBleSdkTestApp/app/src/main/java/com/polar/androidblesdk/MainActivity.kt
@@ -9,7 +9,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import android.content.Intent
-import android.content.Uri
+import android.net.Uri
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
